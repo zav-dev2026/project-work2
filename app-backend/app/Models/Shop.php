fe46186701b1b3f1c3aa4385bfe58d98abc65e50
Giacomo Lin <giacomo.lin@enaipriminifad.org>
2026-07-25 19:16:55 +0200

    aggiunto modelli, lanciata migration

---
+++ app-backend/app/Models/Shop.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Shop extends Model
+{
+    public function shops(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+}
